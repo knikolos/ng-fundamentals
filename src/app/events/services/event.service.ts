113d3fddffaf09ed1fa4ebce099fd2231f38471a
krste_nikoloski <krste.nikoloski@gmail.com>
2020-02-21 00:33:32 +0100

    d

--- src/app/events/services/event.service.ts
+++ src/app/events/services/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IEvent } from '../models/event';
-import { Subject, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ISession } from '../models/session';
 import { catchError } from 'rxjs/operators';
 
@@ -12,27 +12,27 @@ export class EventService {
   constructor(private http: HttpClient) { }
 
   getEvents(): Observable<IEvent[]> {
-    return this.http.get<IEvent[]>("/api/events")
-      .pipe(catchError(this.handleError<IEvent[]>("getEvents", [])));
+    return this.http.get<IEvent[]>('/api/events')
+      .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])));
   }
 
   getEvent(id: number): Observable<IEvent> {
-    return this.http.get<IEvent>("/api/events/" + id)
-      .pipe(catchError(this.handleError<IEvent>("getEvent")));
+    return this.http.get<IEvent>('/api/events/' + id)
+      .pipe(catchError(this.handleError<IEvent>('getEvent')));
   }
 
   saveEvent(event: IEvent): Observable<IEvent> {
-    const options = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
-    return this.http.post<IEvent>("/api/events", event, options)
-      .pipe(catchError(this.handleError<IEvent>("saveEvent")));
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<IEvent>('/api/events', event, options)
+      .pipe(catchError(this.handleError<IEvent>('saveEvent')));
   }
 
   searchSessions(searchTerm: string): Observable<ISession[]> {
-    return this.http.get<ISession[]>("/api/sessions/search?search=" + searchTerm)
-      .pipe(catchError(this.handleError<ISession[]>("searchSessions", [])));
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions', [])));
   }
 
-  private handleError<T>(operation = "operation", result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.log(error);
       return of(result as T);
